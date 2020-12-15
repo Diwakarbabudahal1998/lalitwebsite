f5a2f8fad56ef4dfc36d2bc4ff5c72a3f9c687fa
Diwakarbabudahal1998 <diwakarbd073csit@hcoe.edu.np>
2020-12-15 21:57:06 +0545

    final commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+	
+
+	
+  $('.navbar-nav li a').bind('click', function(event) {
+    $('.navbar-nav li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      },500, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
+
+
+ 
+
+})(jQuery);
